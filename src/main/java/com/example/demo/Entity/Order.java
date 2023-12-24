984ad082c722f66a8143a41a255dd87a98c05e74
Anil7Patil <patilsanil7@gmail.com>
2023-12-24 13:42:16 +0530

    3rd

--- src/main/java/com/example/demo/Entity/Order.java
+++ src/main/java/com/example/demo/Entity/Order.java
@@ -1,5 +1,19 @@
 package com.example.demo.Entity;
 
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="order")
 public class Order {
+	
+	private int id;
+	private Date OrderDateTime;
+	private Date DeliveryDateTime;
+	private double totalPrise;
+	private String orderStatus;
+	
 
 }
--- src/main/java/com/example/demo/Entity/User.java
+++ src/main/java/com/example/demo/Entity/User.java
@@ -1,9 +1,14 @@
 package com.example.demo.Entity;
 
 import jakarta.persistence.Column;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+@Entity
+@Table(name="user")
 public class User {
 	
+	@Id
 	private int id;
 	@Column(name="Name")
 	private String name;
@@ -11,8 +16,38 @@ public class User {
 	private String email;
 	@Column(name="password")
 	private String password;
-	@olumn(name="Address")
+	@Column(name="Address")
 	private String address;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
 	
 	
 	
